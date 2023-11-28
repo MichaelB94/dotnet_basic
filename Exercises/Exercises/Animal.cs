fe18e81cb52fd3c6d358ef57c048da764c08ef94
MichaelB94 <124793101+MichaelB94@users.noreply.github.com>
2023-11-28 11:19:21 +0100

    ex inheritance

---
+++ Exercises/Exercises/Animal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    abstract class Animal
+    {
+        protected string Name {  get; set; }
+        protected int Age { get; set; }
+
+        public Animal(string name,int age) 
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("Animal speaks");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog(string name, int age) : base(name, age) { }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Wooof");
+        }
+
+    }
+
+    class Cat : Animal
+    {
+        public Cat(string name, int age) : base(name, age) { }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+
+    class Horse : Animal
+    {
+        public Horse(string name, int age) : base(name, age) { }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Neigh!");
+        }
+    }
+}
--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -2,63 +2,59 @@
 using System.Diagnostics.Metrics;
 using System;
 using System.IO;
+using Exercises;
 
-//Console.WriteLine("What's your name?");
+Console.WriteLine("What's your name?");
 
-//string? name = Console.ReadLine();
+string? name = Console.ReadLine();
 
-//if (name == null)
-//{
-//    Console.WriteLine("Name not valid.");
-//}
-//else
-//{
-//    Console.WriteLine("Hello, " + name);
-//}
+if (name == null)
+{
+    Console.WriteLine("Name not valid.");
+}
+else
+{
+    Console.WriteLine("Hello, " + name);
+}
 
-//Console.WriteLine("Welcome to AddCalculator.");
-//Console.Write("Enter the first number: ");
-//String firstInput = Console.ReadLine();
+Console.WriteLine("Welcome to AddCalculator.");
+Console.Write("Enter the first number: ");
+String firstInput = Console.ReadLine();
 
-//if (!double.TryParse(firstInput, out double firstNumber))
-//{
-//    Console.WriteLine("Invalid input for the first number.");
-//    return;
-//}
+if (!double.TryParse(firstInput, out double firstNumber))
+{
+    Console.WriteLine("Invalid input for the first number.");
+    return;
+}
 
-//Console.Write("Enter the second number: ");
-//string secondInput = Console.ReadLine();
+Console.Write("Enter the second number: ");
+string secondInput = Console.ReadLine();
 
-//if (!double.TryParse(secondInput, out double secondNumber))
-//{
-//    Console.WriteLine("Invalid input for the second number.");
-//    Console.ReadKey();
-//    return;
-//}
+if (!double.TryParse(secondInput, out double secondNumber))
+{
+    Console.WriteLine("Invalid input for the second number.");
+    Console.ReadKey();
+    return;
+}
 
-//double sum = firstNumber + secondNumber;
-//Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {sum}");
+double sum = firstNumber + secondNumber;
+Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {sum}");
 
-//Console.WriteLine("Welcome to Reverso.");
-//Console.WriteLine("Please write some text, i'll reverse it for you!");
+Console.WriteLine("Welcome to Reverso.");
+Console.WriteLine("Please write some text, i'll reverse it for you!");
 
-//static string ReverseString(string textInput)
-//{
-//    char[] charArray = textInput.ToCharArray();
-//    Array.Reverse(charArray);
-//    return new string(charArray);
-//}
+static string ReverseString(string textInput)
+{
+    char[] charArray = textInput.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
 
-//string? reversedText = ReverseString(Console.ReadLine());
-//Console.WriteLine($"txet desrever : {reversedText}");
+string? reversedText = ReverseString(Console.ReadLine());
+Console.WriteLine($"txet desrever : {reversedText}");
 
-//Console.WriteLine("press any button to close the program");
-//Console.ReadKey();
-
-//Create a program, prompt the user for a file path. Then the program should read that file and print out
-//the contents of that file. What happens when you point it to a text file? What happens when you point it
-//to the PowerPoint file for this unit? Bonus points: make it work for both absolute and relative paths.
-
+Console.WriteLine("press any button to close the program");
+Console.ReadKey();
 
 Console.WriteLine("Welcome to FileReader");
 Console.WriteLine("Enter the file path:");
@@ -74,19 +70,32 @@ string filePath = Console.ReadLine();
  {
     Console.WriteLine($"An error occurred: {ex.Message}");
  }
+static string ReadFile(string path)
+{
+    if (!File.Exists(path))
+    {
+        throw new FileNotFoundException($"File not found: {path}");
+    }
+
+    string fileContent = File.ReadAllText(path);
+    return fileContent;
+}
 
 Console.WriteLine("Press any key to close the program");
 Console.ReadKey();
 
-static string ReadFile(string path)
-{
-    string fullPath = Path.Combine(Environment.CurrentDirectory, path);
+Console.WriteLine("Welcome to AnimalSound");
 
-    if (!File.Exists(fullPath))
-    {
-        throw new FileNotFoundException($"File not found: {fullPath}");
-    }
+Dog myDog = new Dog("Loki", 3);
+Cat myCat = new Cat("Thor", 2);
+Horse myHorse = new Horse("Hulk", 5);
 
-    string fileContent = File.ReadAllText(fullPath);
-    return fileContent;
-}
+myDog.Speak();
+myCat.Speak();
+myHorse.Speak();
+
+
+Console.WriteLine("Press any key to close the program");
+Console.ReadKey();
+
+
